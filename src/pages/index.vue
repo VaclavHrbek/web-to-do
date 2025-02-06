e3164f210e231912b057f51f56b266bcb254e204
VaclavHrbek <st43190@student.upce.cz>
2025-02-06 22:40:31 +0100

    Improve layout of index page.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+</script>
+
 <template>
-    <div class="bg-red-500">
-      Welcome to Home page.
-    </div>
-  </template>
-  
+  <div class="bg-slate-900/50 grid grid-cols-1 grid-row-1 justify-items-center content-center h-screen w-screen">
+<button type="button" class="bg-slate-800/50 size-64 rounded-3xl border-8 border-emerald-950 hover:border-emerald-900">
+  <div class="text-emerald-700 text-4xl font-bold">
+    To Do
+  </div>
+</button>
+  </div>
+</template>
