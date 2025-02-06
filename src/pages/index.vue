888bbce559e7dc80170a56b16210f4f68773b56c
VaclavHrbek <st43190@student.upce.cz>
2025-02-06 22:55:52 +0100

    Add link to the todo on main page.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,10 +6,11 @@ definePageMeta({
 
 <template>
   <div class="bg-slate-900/50 grid grid-cols-1 grid-row-1 justify-items-center content-center h-screen w-screen">
-<button type="button" class="bg-slate-800/50 size-64 rounded-3xl border-8 border-emerald-950 hover:border-emerald-900">
-  <div class="text-emerald-700 text-4xl font-bold">
-    To Do
-  </div>
-</button>
+    <NuxtLink to="/tasks" class="flex justify-center items-center bg-slate-800/50 size-64 rounded-3xl border-8
+       border-emerald-950 hover:border-emerald-900">
+      <div class="text-emerald-700 text-4xl font-bold">
+        To Do
+      </div>
+    </NuxtLink>
   </div>
 </template>
